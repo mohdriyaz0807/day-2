9be0b50b86b43dcfcc68ff5cac692b99820fd1e8
mohdriyaz0807 <72142827+mohdriyaz0807@users.noreply.github.com>
2020-10-13 11:49:44 +0530

    Add files via upload

---
+++ flag_url.js
@@ -0,0 +1,15 @@
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+
+request.send();
+
+request.onload=function(){
+
+    var data  = JSON.parse(this.responseText);
+    console.log(data)
+    for(var i in data)
+    {
+        console.log(data[i].flag)
+    }
+}
